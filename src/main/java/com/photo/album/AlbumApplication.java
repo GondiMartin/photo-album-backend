dacb682323bf94504c86cf4bd1d59c0e7bbc5b0c
martin.gondocs@gmail.com <martin.gondocs@gmail.com>
2025-03-30 14:17:02 +0200

    Add Hello World

--- src/main/java/com/photo/album/AlbumApplication.java
+++ src/main/java/com/photo/album/AlbumApplication.java
@@ -2,12 +2,20 @@ package com.photo.album;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AlbumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlbumApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home(){
+		return "Hello World!";
 	}
 
 }
